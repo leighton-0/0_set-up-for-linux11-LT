744a961fc42dde5489d3eb733d5c14aa2d8a16b3
Leighton Clucas <63763567+leighton-0@users.noreply.github.com>
2023-01-05 06:12:38 +0000

    Update tmux_startup.sh

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -6,7 +6,7 @@
 
 # this script sets up a new tmux server with a standard layout of 3 windows each splt with a 
 
-tmux new-session -s server hello
+tmux new-session -s server_hello
 #tmux new-window -c '#{pane_current_path}'
 #tmux new-window -c "#{pane_current_path}"
 #tmux rename-window -t work vim
