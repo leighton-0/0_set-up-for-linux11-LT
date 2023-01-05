4836ecb43e9307ad6d82e90e399918a81b820e60
Leighton Clucas <63763567+leighton-0@users.noreply.github.com>
2023-01-05 05:59:10 +0000

    Update tmux_startup.sh

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -7,9 +7,9 @@
 # this script sets up a new tmux server with a standard layout of 3 windows each splt with a 
 
 tmux new-session -s hello
-new-window -c "#{pane_current_path}"
-new-window -c "#{pane_current_path}"
-split-window -c "#{pane_current_path}"
+tmux new-window -c "#{pane_current_path}"
+tmux new-window -c "#{pane_current_path}"
+tmux split-window -c "#{pane_current_path}"
 #tmux new-window -c "#{pane_current_path}"
 #tmux rename-window -t work vim
 #tmux new-window -c "#{pane_current_path}"
@@ -26,7 +26,7 @@ split-window -c "#{pane_current_path}"
 # ===  To run the script ===
 # ==========================
 
-# curl -k -s https://raw.githubusercontent.com/leighton-0/Standard-set-ups/main/tmux_startup | bash
+# curl -k -s https://raw.githubusercontent.com/leighton-0/Standard-set-ups/main/tmux_startup.sh | bash
 # The -k, --insecure flag allows curl to proceed and operate even for server connections otherwise considered insecure
 # The _s means silent Silent or quiet mode. Don't show progress meter or error messages.  Makes Curl mute
 
