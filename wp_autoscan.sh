af1ec231d7a0b8ad4c881f76d2950dfe7fe7dea0
Leighton Clucas <63763567+leighton-0@users.noreply.github.com>
2023-01-10 15:45:37 +0000

    Update wp_autoscan.sh

--- wp_autoscan.sh
+++ wp_autoscan.sh
@@ -1,8 +1,12 @@
 wpscan --update
-read -p $'\e[1;92m Liste mit Wordpresswebistes: \e[0m' wl_pass 
+read -p $'\e[1;92m Liste mit Wordpresswebistes: \e[0m' wl_pass
+# -p prompt asks for a file name for the list of sites - names this site wl_pass
+
 wl_pass="${wl_pass}"
+# creates a variable called wp_pass
 
 count_pass=$(wc -l $wl_pass | cut -d " " -f1)
+# counts the number of lines in wp_pass file
 
 for fn in `cat $wl_pass`; do
 wpscan --url $fn -e  u,vp  >> result
